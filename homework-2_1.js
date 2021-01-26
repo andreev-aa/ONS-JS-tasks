0a9ea25e82fecc25b53edd3ca239e683cdec2a82
andreev-aa <andreev.andrey10@gmail.com>
2021-01-26 23:17:31 +0300

    hw2_1-complete

--- homework-2_1.js
+++ homework-2_1.js
@@ -1,4 +1,4 @@
-//задача 1
+// Задача 1
 function checkLength(a) {
     let b = a.length;
     if (b < 10) {
@@ -7,5 +7,16 @@ function checkLength(a) {
         console.log(`Слово "${a}" слишком длинное!`);
     }
 }
-
 checkLength('Последовательность');
+
+// Задача 2
+function concatTwoStrings(string1, string2) {
+    const newString = `${string1}${string2}`;
+    if (newString.length > 20) {
+        return newString.substring(0, 20) + '...';
+    } else {
+        return newString;
+    }
+}
+
+console.log(concatTwoStrings('На золтом крыльце ', '20aaa'));
